e486a8c57c04c1421159fa6f6e182946a05fd950
Dharshansk16 <dcodecraft@gmail.com>
2024-07-06 12:31:27 +0530

    feat:Added Accordion from shadcn/ui

---
+++ src/components/Chat.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { Accordion, AccordionItem, AccordionTrigger } from "./ui/accordion";
+
+const Chat: FC = () => {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="relative bg-white z-40 shadow"
+    >
+      <AccordionItem value="item-1">
+        <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 overflow:hidden">
+          <div className="w-full h-full flex flex-col">
+            <AccordionTrigger className="px-6 border-b  border-zinc-300">
+              smart AI
+            </AccordionTrigger>
+          </div>
+        </div>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default Chat;
